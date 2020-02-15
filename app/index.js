9b6dc43d3bbba3669cd4a3d0a9fb80ed7a1493f9
artemshchurev <artemshchurev@gmail.com>
2020-02-15 18:04:11 +1000

    query string

--- app/index.js
+++ app/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
+import Results from './components/Results'
 import { ThemeProvider } from './contexts/theme'
 import Nav from './components/Nav'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends React.Component {
     constructor(props) {
@@ -21,15 +23,19 @@ class App extends React.Component {
     }
     render() {
         return (
-            <ThemeProvider value={this.state}>
-                <div className={this.state.theme}>
-                    <div className='container'>
-                        <Nav />
+            <Router>
+                <ThemeProvider value={this.state}>
+                    <div className={this.state.theme}>
+                        <div className='container'>
+                            <Nav />
 
-                        <Popular />
+                            <Route exact path='/' component={Popular} />
+                            <Route exact path='/battle' component={Battle} />
+                            <Route path='/battle/results' component={Results} />
+                        </div>
                     </div>
-                </div>
-            </ThemeProvider>
+                </ThemeProvider>
+            </Router>
         )
     }
 }
